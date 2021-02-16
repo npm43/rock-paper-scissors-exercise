9ab9dbaf38d7d5f4185900ad61e5df1faf13d3bb
npm43 <npm43@georgetown.edu>
2021-02-15 20:39:52 -0500

    Ask user for an input

--- game.py
+++ game.py
@@ -1,3 +1,54 @@
 # game.py
 
-print("Rock, Paper, Scissors, Shoot!")
+
+
+
+print("-------------------")
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print("-------------------")
+
+#
+#asking user for an input
+#
+
+x = input("Please choose either 'rock', 'paper', or 'scissors':")
+
+print("You chose:", x)
+
+
+
+
+
+
+
+exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#simulating a computer input
+#
+
+print("The computer chose: 'paper'")
+
+#
+#determining who won
+#
+
+print("-------------------")
+print("Oh, the computer won. It's ok.")
+print("-------------------")
+print("Thanks for playing. Please play again!")
