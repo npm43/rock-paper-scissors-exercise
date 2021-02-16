4afb08d1d3eb2692f3693be3fb53de2e1c79432b
npm43 <npm43@georgetown.edu>
2021-02-15 21:48:54 -0500

    Validate user inputs

--- game.py
+++ game.py
@@ -11,42 +11,59 @@ print("-------------------")
 #
 
 user_choice = input("Please choose either 'rock', 'paper', or 'scissors':")
+print("-------------------")
 
 print("You chose:", user_choice)
+
+#
+#validate the user selection
+#
+
+options = ["rock", "paper", "scissors"]
+user_choice.lower()
+
+if user_choice in options:
+    pass
+else:
+    print("Oops. Please choose a valid option and try again.")
+    exit()
 
 #
 #simulating a computer input
 #
 
-options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(options)
 
 print("The computer chose:", computer_choice)
-
-
-
-
-
-
-
-
-
-
-
-exit()
-
-
-
-
-
-
-
 
 #
 #determining who won
 #
 
 print("-------------------")
-print("Oh, the computer won. It's ok.")
+if user_choice == "rock":
+    if computer_choice == "rock":
+        print("Oh, it's a tie.")
+    elif computer_choice == "paper":
+        print("Oh, the computer won. It's ok.")
+    elif computer_choice == "scissors":
+        print("Oh, you won! Nice job.")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("Oh, you won! Nice job.")
+    elif computer_choice == "paper":
+        print("Oh, it's a tie.")
+    elif computer_choice == "scissors":
+        print("Oh, the computer won. It's ok.")
+elif user_choice == "scissors":
+    if computer_choice == "rock":
+        print("Oh, the computer won. It's ok.")
+    elif computer_choice == "paper":
+        print("Oh, you won! Nice job.")
+    elif computer_choice == "scissors":
+        print("Oh, it's a tie.")
+else:
+    print("OOPS SOMETHING WENT WRONG.")
+
 print("-------------------")
 print("Thanks for playing. Please play again!")
